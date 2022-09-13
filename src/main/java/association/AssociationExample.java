083ada3b4af9dc95ff3b600641618ab6539748c3
Mihir Sawant <mihirssawant06@gmail.com>
2022-09-14 00:23:13 +0530

    added department class in association

--- src/main/java/association/AssociationExample.java
+++ src/main/java/association/AssociationExample.java
@@ -8,8 +8,19 @@ public class AssociationExample {
         Faculty two = new Faculty("Vijeytha Mam");
         Course dbms = new Course("DBMS", 4.5f);
 
+        Department mca = new Department("MCA");
+        Department mbaTech = new Department("MBA_TECH");
+
+        Faculty os = new Faculty("Sofia Mam");
+
         // One to One Relationship
         System.out.println("Faculty "+one.getName()+" Teaches "+dsa.getName());
         System.out.println("Faculty "+two.getName()+" Teaches "+dbms.getName());
+
+
+        // One To Many Relationship
+        System.out.println("Faculty "+os.getName()+" is Part of "+mca.getName()+" Department");
+        System.out.println("Faculty "+os.getName()+" is Part of "+ mbaTech.getName()+" Department");
+
     }
 }

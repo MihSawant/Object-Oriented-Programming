6abe1f4b661a37dadd07df25bce71c944240dcfb
Mihir Sawant <mihirssawant06@gmail.com>
2022-09-16 22:28:49 +0530

    Solid - open closed principal impl code

---
+++ src/main/java/solid/o/StudentServ.java
@@ -0,0 +1,13 @@
+package solid.o;
+
+/* Created Interface StudentServ so that
+next time if we add a method in
+ interface then will implement it accordingly
+ */
+public interface StudentServ {
+    void calculateAverageAttendance();
+
+    void uploadEmailNewsletter();
+
+    void coursesList();
+}
---
+++ src/main/java/solid/o/StudentServiceImpl.java
@@ -0,0 +1,20 @@
+package solid.o;
+
+// As per the requirements, we can implement the code here
+public class StudentServiceImpl implements StudentServ{
+
+    @Override
+    public void calculateAverageAttendance() {
+
+    }
+
+    @Override
+    public void uploadEmailNewsletter() {
+
+    }
+
+    @Override
+    public void coursesList() {
+
+    }
+}

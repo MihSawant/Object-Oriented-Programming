d170c5cb3c478c1962b9dc37b8963134c129dbe9
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-08 11:43:24 +0530

    constructor overloading in other constructor

--- src/main/java/misc/BankAccount.java
+++ src/main/java/misc/BankAccount.java
@@ -5,8 +5,12 @@ public class BankAccount {
     double balance;
 
     // Constructor Overloading
+
+    // In case of zero balance accounts
+
+    // constructors use other constructors to initialize values
     public BankAccount(String accountHolderName){
-        name = accountHolderName;
+        new BankAccount(accountHolderName, 0.0);
     }
 
     public BankAccount(String accountHolderName, double openingAmount){

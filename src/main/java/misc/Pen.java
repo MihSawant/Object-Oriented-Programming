c21f52866cb1b1db86af0b1459c47f3b38823510
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-08 11:35:55 +0530

    default constructor

---
+++ src/main/java/misc/Pen.java
@@ -0,0 +1,17 @@
+package misc;
+
+public class Pen {
+    String inkColour;
+    String name;
+    enum Type{
+        GEL,
+        BALL,
+        FOUNTAIN
+    }
+    // No constructor for this class
+    /*
+     */
+    public static void main(String[] args) {
+        new Pen();
+    }
+}
